c1aa8693439fd0b2d767096c6b21bd726211e041
Matt Pearce <matt@flax.co.uk>
2013-12-20 13:48:32 +0000

    Tidy entity extractor, remove redundant classes, add SearchResults.

--- ukmp/java/ukmp/src/main/java/uk/co/flax/ukmp/UKMPConfiguration.java
+++ ukmp/java/ukmp/src/main/java/uk/co/flax/ukmp/UKMPConfiguration.java
@@ -15,9 +15,6 @@
  */
 package uk.co.flax.ukmp;
 
-import java.util.Map;
-
-import uk.co.flax.ukmp.api.Party;
 import uk.co.flax.ukmp.config.EntityConfiguration;
 import uk.co.flax.ukmp.config.SolrConfiguration;
 
@@ -29,49 +26,10 @@ import com.yammer.dropwizard.config.Configuration;
  */
 public class UKMPConfiguration extends Configuration {
 
-	@JsonProperty("data_dir")
-	private String dataDir;
-	@JsonProperty("archive_dir")
-	private String archiveDir;
-
-	@JsonProperty("twitter")
-	private Map<String, String> twitterProperties;
-
-	@JsonProperty("party_lists")
-	private Map<String, Party> partyMap;
-
 	@JsonProperty("solr")
 	private SolrConfiguration solrConfiguration;
 	@JsonProperty("entity")
 	private EntityConfiguration entityConfiguration;
-
-	/**
-	 * @return the dataDir
-	 */
-	public String getDataDir() {
-		return dataDir;
-	}
-
-	/**
-	 * @return the archiveDir
-	 */
-	public String getArchiveDir() {
-		return archiveDir;
-	}
-
-	/**
-	 * @return the twitterProperties
-	 */
-	public Map<String, String> getTwitterProperties() {
-		return twitterProperties;
-	}
-
-	/**
-	 * @return the partyMap
-	 */
-	public Map<String, Party> getPartyMap() {
-		return partyMap;
-	}
 
 	public SolrConfiguration getSolrConfiguration() {
 		return solrConfiguration;
---
+++ ukmp/java/ukmp/src/main/java/uk/co/flax/ukmp/api/SearchResults.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2013 Lemur Consulting Ltd.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.co.flax.ukmp.api;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Set of results returned from a search.
+ */
+public class SearchResults {
+
+	private final int start;
+	private final int numResults;
+	private final String query;
+	private final String sortOrder;
+
+	/** The facets that can be applied to this results list */
+	private final Map<String, Facet> facets;
+
+	/** The filters already applied */
+	private final Map<String, List<String>> appliedFilters;
+
+	private final List<Tweet> tweets;
+	private final String errorMessage;
+
+	public SearchResults(int start, int numResults, List<Tweet> results, String query,
+			Map<String, List<String>> filters, String sortOrder, Map<String, Facet> facets) {
+		this(start, numResults, results, query, filters, sortOrder, facets, null);
+	}
+
+	public SearchResults(String error) {
+		this(0, -1, null, null, null, null, null, error);
+	}
+
+	public SearchResults(int start, int numResults, List<Tweet> results, String query,
+			Map<String, List<String>> filters, String sortOrder, Map<String, Facet> facets, String error) {
+		this.start = start;
+		this.numResults = numResults;
+		this.tweets = results;
+		this.query = query;
+		this.appliedFilters = filters;
+		this.sortOrder = sortOrder;
+		this.facets = facets;
+		this.errorMessage = error;
+	}
+
+	/**
+	 * @return the index of the first result in the set.
+	 */
+	public int getStart() {
+		return start;
+	}
+
+	/**
+	 * @return the total number of results for the search.
+	 */
+	public int getNumResults() {
+		return numResults;
+	}
+
+	/**
+	 * @return the query string for the search.
+	 */
+	public String getQuery() {
+		return query;
+	}
+
+	/**
+	 * @return the filters applied to the search results.
+	 */
+	public Map<String, List<String>> getAppliedFilters() {
+		return appliedFilters;
+	}
+
+	/**
+	 * @return the sort order of the search results.
+	 */
+	public String getSortOrder() {
+		return sortOrder;
+	}
+
+	/**
+	 * @return the tweets returned by the search.
+	 */
+	public List<Tweet> getTweets() {
+		return tweets;
+	}
+
+	/**
+	 * @return the facets that may be applied to this search.
+	 */
+	public Map<String, Facet> getFacets() {
+		return facets;
+	}
+
+	/**
+	 * @return any error message that was returned by the search.
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+}
