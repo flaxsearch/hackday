31ed2ac5cd2cd92bd5af60438f01d4c7c6158b1b
Tom Mortimer <tom@flax.co.uk>
2014-01-16 15:13:28 +0000

    configurable refetch

--- ukmp/scripts/fetch_lists.py
+++ ukmp/scripts/fetch_lists.py
@@ -25,20 +25,25 @@ import yaml
 import time
 
 def read_since(datadir, username):
-    """Read the user's since value from their since file in the data directory.
-    Returns 0 if the file does not exist."""
+    """Read the user's since values from their since file in the data directory.
+    
+    The since file stores a list of up to 100 max_id values in ascending order. We can
+    then use this to periodically reindex tweets already seen.
+    
+    Returns [0] if the file does not exist.
+    """
     filepath = os.path.join(datadir, username + '.since')
     if not os.path.exists(filepath):
-        return 0
+        return [0]
     else:
         with open(filepath) as f:
             data = f.read()
-        return long(data) if data else 0
+        return [long(x) for x in data.split()] if data else [0]
 
 def write_since(datadir, username, since):
-    """Write the user's since value to the since file in the data directory."""
+    """Write the user's since values to the since file in the data directory."""
     with open(os.path.join(datadir, username + '.since'), 'w') as f:
-        f.write(str(since))
+        f.write('\n'.join(str(x) for x in since))
 
 def get_api(config):
     return twitter.Api(consumer_key=config['twitter']['consumer_key'],
@@ -52,8 +57,13 @@ def handle_stats(data_dir, user, stats, since):
         for s in stats:
             max_id = max(max_id, s.id)
             f.write(json.dumps(s.AsDict()) + '\n')
-    if max_id > since:
-        write_since(data_dir, user, max_id)
+
+    if max_id > since[-1]:
+        if since == [0]: 
+            since = []
+        # add the latest since value and truncate the list
+        since = (since + [max_id])[config['reindexes'][0]:]
+        write_since(data_dir, user, since)
 
 
 if len(sys.argv) != 2:
@@ -71,13 +81,36 @@ for party in config['party_lists']:
     party_config = config['party_lists'][party]
     slug = party_config['twitter_list_slug']
     since = read_since(config['data_dir'], slug)
+
+    # reindexes in config should look something like [-48, -4, -1]
+    # it should always include -1!
+    reindexes = config['reindexes']
     try:
-        stats = api.GetListTimeline(list_id=None, slug=slug, 
-            owner_screen_name=party_config['twitter_screen_name'], 
-            since_id=since, count=config['twitter']['tweet_count'])
-        print "Got {2} tweets for {0} since {1}".format(slug, since, len(stats))
-        handle_stats(config['data_dir'], slug, stats, since)
+        all_stats = {}
+        # start with earlier since values (re-indexing tweets)
+        # FIXME this is a pretty dumb algorithm
+        for reindex in reindexes:
+            try:
+                since_id = since[reindex]
+                stats = api.GetListTimeline(list_id=None, slug=slug, 
+                    owner_screen_name=party_config['twitter_screen_name'], 
+                    since_id=since_id, 
+                    count=config['twitter']['tweet_count'],
+                )
+                print "Got {2} tweets for {0} since {1}".format(slug, since_id, len(stats))
+                
+                # merge into all_stats
+                for s in stats:
+                    all_stats[s.id] = s
+                    
+                time.sleep(config['twitter']['delay'])
+
+            except IndexError as e:
+                pass
+
+            handle_stats(config['data_dir'], slug, all_stats.values(), since)
+
     except twitter.TwitterError as e:
         print "%s: Caught TwitterError '%s'" % (slug, e)
-    time.sleep(config['twitter']['delay'])
+
 
