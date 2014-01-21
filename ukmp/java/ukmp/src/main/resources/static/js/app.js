055da82923c0e0aab17ff15d66e9a03aecc840b6
Matt Pearce <matt@flax.co.uk>
2014-01-21 19:02:43 +0000

    Undo previous change, make searches from wordcloud work again.

--- ukmp/java/ukmp/src/main/resources/static/js/app.js
+++ ukmp/java/ukmp/src/main/resources/static/js/app.js
@@ -5,6 +5,9 @@ ukmpApp.config([ '$routeProvider',
 		$routeProvider.when('/search', {
 			templateUrl : 'template/search.html',
 			controller : 'UKMP_SearchCtrl'
+		}).when('/search/:query', {
+			templateUrl : 'template/search.html',
+			controller : 'UKMP_SearchCtrl'
 		}).when('/about', {
 			templateUrl : 'template/about.html',
 			controller : 'UKMP_AboutCtrl'
