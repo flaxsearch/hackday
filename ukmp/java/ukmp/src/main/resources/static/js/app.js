e8c5cef3e6408e677d28f594ae267b746659cf09
Matt Pearce <matt@flax.co.uk>
2014-01-21 14:05:11 +0000

    Fix issue where browse page not loading first time.

--- ukmp/java/ukmp/src/main/resources/static/js/app.js
+++ ukmp/java/ukmp/src/main/resources/static/js/app.js
@@ -3,7 +3,8 @@ var ukmpApp = angular.module('ukmpApp', [ 'ngRoute', 'ukmpControllers' ]);
 ukmpApp.config([ '$routeProvider', 
 	function($routeProvider) {
 		$routeProvider.when('/search', {
-			templateUrl : 'template/search.html'
+			templateUrl : 'template/search.html',
+			controller : 'UKMP_SearchCtrl'
 		}).when('/search/:query', {
 			templateUrl : 'template/search.html',
 			controller : 'UKMP_SearchCtrl'
